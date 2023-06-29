da722bfa169cef23356816b67b915b4068f232c0
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-28 22:13:03 -0300

    finished Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createToDo } from "../redux/actions";
 
 const Form = (props) => {
+  const dispatch = useDispatch();
+
+  const initialState = {
+    text: "",
+    complete: false,
+  };
+
+  const [form, setForm] = useState(initialState);
+
+  const [id, setId] = useState(0);
+
+  const changeHandler = (event) => {
+    setForm({
+      ...form,
+      text: event.target.value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(createToDo({ ...form, id }));
+    setForm(initialState);
+    setId(id + 1);
+  };
+
   return (
     <>
-      <h1>Form</h1>
+      <h1>Create Task</h1>
+      <form onSubmit={submitHandler}>
+        <label>New Task: </label>
+        <input
+          type="text"
+          value={form.text}
+          placeholder="Write a task..."
+          onChange={changeHandler}
+        />
+        <button type="submit">Add Task</button>
+      </form>
     </>
   );
 };
--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
 
 const ToDos = (props) => {
   return (
